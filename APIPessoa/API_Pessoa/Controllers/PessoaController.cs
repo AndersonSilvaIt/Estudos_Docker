e3576116ed0c048556d65bb40e0fa49ef2c18511
anderson.silvait <anderson.silvait@outlook.com>
2024-04-24 21:36:25 -0300

    Adicionado Controller de pessoa

---
+++ APIPessoa/API_Pessoa/Controllers/PessoaController.cs
@@ -0,0 +1,23 @@
+﻿using API_Pessoa.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Pessoa.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<PessoaVM>> Obter()
+        {
+            List<PessoaVM> listPessoa = new List<PessoaVM>
+            {
+                new PessoaVM{ Id = 1, Nome = "Pessoa 1", Idade = 15 },
+                new PessoaVM{ Id = 2, Nome = "Pessoa 2", Idade = 18 },
+                new PessoaVM{ Id = 3, Nome = "Pessoa 3", Idade = 25 }
+            };
+
+            return Ok(listPessoa);
+        }
+    }
+}
